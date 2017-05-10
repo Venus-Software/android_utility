6b3060ff59d8ed1b9ca586d0869de30a73a485a6
yangxd <hihi>
2017-05-10 09:54:53 +0800

    2017/05/10  9:54

--- AndroidUtils/utilslibs/src/main/java/com/vic/utils/ImageUtils.java
+++ AndroidUtils/utilslibs/src/main/java/com/vic/utils/ImageUtils.java
@@ -21,7 +21,7 @@ public class ImageUtils {
         if (degree != 0) {//旋转照片角度，防止头像横着显示
             bm = rotateBitmap(bm, degree);
         }
-        File outputFile = new File(targetPath);
+        File outputFile = new File( targetPath);
         try {
             if (!outputFile.exists()) {
                 outputFile.getParentFile().mkdirs();
